6c3498a927e162d16bc8d092d2c739a711800472
Francisco Gonzalez <gzmorell@gmail.com>
2022-11-01 14:22:48 +0100

    Short-fibonacci solution

--- rust/short-fibonacci/src/lib.rs
+++ rust/short-fibonacci/src/lib.rs
@@ -1,13 +1,45 @@
 /// Create an empty vector
 pub fn create_empty() -> Vec<u8> {
-    unimplemented!()
+    vec![]
 }
 
 /// Create a buffer of `count` zeroes.
 ///
 /// Applications often use buffers when serializing data to send over the network.
 pub fn create_buffer(count: usize) -> Vec<u8> {
-    unimplemented!("create a zeroized buffer of {} bytes", count)
+    vec![0; count]
+}
+
+struct Fib {
+    first: Option<u8>,
+    second: Option<u8>,
+}
+
+impl Fib {
+    fn new() -> Fib {
+        Fib {
+            first: Some(1),
+            second: Some(1),
+        }
+    }
+}
+
+impl Iterator for Fib {
+    type Item = u8;
+    fn next(&mut self) -> Option<Self::Item> {
+        let result = self.first;
+        if let Some(x) = result {
+            self.first = self.second;
+            if let Some(y) = self.second {
+                if (x as usize + y as usize) > Self::Item::MAX as usize {
+                    self.second = None;
+                } else {
+                    self.second = Some(x + y);
+                }
+            }
+        }
+        result
+    }
 }
 
 /// Create a vector containing the first five elements of the Fibonacci sequence.
@@ -15,5 +47,5 @@ pub fn create_buffer(count: usize) -> Vec<u8> {
 /// Fibonacci's sequence is the list of numbers where the next number is a sum of the previous two.
 /// Its first five elements are `1, 1, 2, 3, 5`.
 pub fn fibonacci() -> Vec<u8> {
-    unimplemented!()
+    Fib::new().take(5).collect()
 }
